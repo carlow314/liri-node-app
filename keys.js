30afb7ab8934c60f6432c887e5c0772755f61831
Jeff Carlow <jeff.carlow@gmail.com>
2017-07-15 13:30:46 -0600

    core functionality complete

---
+++ keys.js
@@ -0,0 +1,15 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '2eWNKEu09afDGIXVZKSJlHVh6',
+  consumer_secret: 'up6JgLhDbqlUYbqbhTEV0d5IBcqGH1FYxA6TeTR3Dq2OMUCVpc',
+  access_token_key: '30546298-RsJ1Lha36dh3wid46vnd2rcqmgpqsGFIAwLliHEJk',
+  access_token_secret: 'tMMCcIntFX8MnKThJli0071a3X4zSIugSlqjGc8JEujmw'
+};
+exports.spotifyKeys = {
+  id: '5a01047e306f4407b02f8cc11c639aab',
+  secret: 'aa9cbb16853744b68e9303cb5c85821f',
+};
+exports.omdbKey = {
+  id: '40e9cece',
+};
